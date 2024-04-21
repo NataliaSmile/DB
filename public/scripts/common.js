68887a004b1a9cb61d308d3e85412e6e94058fc3
NataliaSmile <lutsyshyna.n@donnu.edu.ua>
2024-04-21 14:49:05 +0300

    inital commit

---
+++ public/scripts/common.js
@@ -0,0 +1,131 @@
+const ENDPOINTS = {
+    WORDS: '/words/',
+    LEVELS: `/words/levels/`,
+    CHARACTERISTICS: '/words/characteristics/',
+}
+const query = {
+    level: null,
+    char: null,
+    search: null,
+}
+
+function getQueryParams() {
+    const arr = [
+        query.level ? `level=${query.level}` : null,
+        query.char ? `char=${query.char}` : null,
+        query.search ? `search=${query.search}` : null
+    ];
+
+    const filteredArray = arr.filter(item => item !== null);
+    const res = filteredArray.join("&");
+
+    return res.length > 0 ? `?${res}` : ""
+}
+
+async function get(endpoint) {
+    return new Promise((resolve, reject) => {
+        fetch(endpoint + getQueryParams()).then(response => resolve(response.json()))
+            .catch(error => console.error('Error fetching items:', error));
+    })
+}
+
+function wordsToTable(words) {
+    const table = document.querySelector('#table tbody');
+    table.innerHTML = '';
+
+    words.forEach(word => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${word.id}</td>
+            <td>${word.name}</td>
+            <td>${word.description}</td>
+            <td>${word.level}</td>
+            <td>${word.characteristic}</td>
+        `;
+        table.appendChild(row);
+    });
+}
+
+function toggleDropdown(id) {
+    const dropdownMenu = document.getElementById(id);
+    dropdownMenu.classList.toggle("show");
+}
+
+function fillDropdown(dropDownId, titles, onclickFunc) {
+    const drop = document.getElementById(dropDownId);
+    let items = '';
+
+    titles.forEach(title => {
+        items += `<a href="#" onclick="${onclickFunc}('${title}')">${title}</a>`;
+    });
+
+    drop.innerHTML = items;
+}
+
+async function setLevel(level) {
+    query.level = level;
+    const words = await get(ENDPOINTS.WORDS)
+    wordsToTable(words)
+
+    document.getElementById("drop-btn-1").innerText = "Рівень: " + level
+}
+
+async function setChar(char) {
+    query.char = char;
+    const words = await get(ENDPOINTS.WORDS)
+    wordsToTable(words)
+
+    document.getElementById("drop-btn-2").innerText = "Характеристика: " + char
+}
+
+async function search() {
+    const input = document.getElementById('search')
+    query.search = input.value;
+    const words = await get(ENDPOINTS.WORDS)
+    wordsToTable(words)
+}
+
+
+async function resetFilters() {
+    query.level = null;
+    query.char = null;
+    query.search = null;
+
+    const words = await get(ENDPOINTS.WORDS)
+    wordsToTable(words)
+
+    document.getElementById("drop-btn-1").innerText = "За рівнем"
+    document.getElementById("drop-btn-2").innerText = "За морфологічною характеристикою"
+    document.getElementById('search').value = '';
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    document.addEventListener('click', (event) => {
+        const header = document.getElementById('header');
+        const nav = document.getElementById('navigation');
+        const table = document.getElementById('table');
+        const footer = document.getElementById('footer');
+        header.classList.add('small');
+        footer.classList.add('hidden');
+        table.classList.remove('hidden');
+        nav.classList.remove('hidden');
+
+        // Hide dropdowns
+        if (!event.target.matches('.dropbtn')) {
+            const dropdowns = document.getElementsByClassName("dropdown-content");
+            for (let i = 0; i < dropdowns.length; i++) {
+                const openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) openDropdown.classList.remove('show');
+            }
+        }
+    });
+
+    const words = await get(ENDPOINTS.WORDS);
+    const levels = await get(ENDPOINTS.LEVELS)
+    const characteristics = await get(ENDPOINTS.CHARACTERISTICS)
+
+    fillDropdown('dropdown-1', levels, 'setLevel')
+    fillDropdown('dropdown-2', characteristics, 'setChar')
+
+    wordsToTable(words)
+});
---
+++ server.py
@@ -0,0 +1,72 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from pydantic import BaseModel
+from pathlib import Path
+from fastapi.staticfiles import StaticFiles
+
+
+DATABASE_URL = "sqlite:///./db/database.db"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class Word(BaseModel):
+    name: str
+    description: str
+    level: str
+    characteristic: str
+
+class WordModel(Base):
+    __tablename__ = "words"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String)
+    level = Column(String)
+    characteristic= Column (String)
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def create_sql_query(level: str, char: str, search: str):
+    arr = [
+        f"level = '{level}'" if level else None,
+        f"characteristic = '{char}'" if char else None,
+        f"name LIKE '%{search}%'" if search else None
+    ]
+    filtered_array = [item for item in arr if item is not None]
+    res = " AND ".join(filtered_array)
+    return  f"SELECT * FROM words{' WHERE ' + res if len(res) > 0 else ''}"
+
+@app.get("/words/")
+def read_words(db: Session = Depends(get_db), level: str = None, char: str = None, search: str = None):
+    sql = create_sql_query(level, char, search)
+    result = db.execute(sql)
+    response = []
+    for row in result:
+        response.append(row)
+    return response
+
+@app.get("/words/levels/")
+def read_levels(db: Session = Depends(get_db)):
+    result = db.execute("SELECT DISTINCT level FROM words")
+    levels = [row[0] for row in result]
+    return levels
+
+@app.get("/words/characteristics/")
+def read_characteristics(db: Session = Depends(get_db)):
+    result = db.execute("SELECT DISTINCT characteristic FROM words")
+    characteristics = [row[0] for row in result]
+    return characteristics
+
+static_dir = Path(__file__).parent / "public"
+app.mount("/", StaticFiles (directory="public", html=True), name="public")
